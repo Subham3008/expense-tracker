5e3ae129dd72c5e49d0dfbdb077d1dce1bfe9701
Subham3008 <subhamsamanta065@gmail.com>
2026-06-09 17:38:58 +0530

    feat: add expense filters

--- api/src/controllers/expenseController.js
+++ api/src/controllers/expenseController.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import Expense from '../models/Expense.js';
+import Expense, { EXPENSE_CATEGORIES } from '../models/Expense.js';
 
 const sendError = (res, statusCode, message) => {
   res.status(statusCode).json({ message });
@@ -7,13 +7,70 @@ const sendError = (res, statusCode, message) => {
 
 const isValidObjectId = (id) => mongoose.Types.ObjectId.isValid(id);
 
-export const getExpenses = async (_req, res) => {
+const parseDateFilter = (value, fieldName) => {
+  if (!value) {
+    return null;
+  }
+
+  const date = new Date(value);
+
+  if (Number.isNaN(date.getTime())) {
+    const error = new Error(`${fieldName} must be a valid date`);
+    error.statusCode = 400;
+    throw error;
+  }
+
+  return date;
+};
+
+const buildExpenseFilters = ({ category, from, to }) => {
+  const filters = {};
+
+  if (category) {
+    if (!EXPENSE_CATEGORIES.includes(category)) {
+      const error = new Error(
+        'Category must be one of: Food, Transport, Bills, Entertainment, Other',
+      );
+      error.statusCode = 400;
+      throw error;
+    }
+
+    filters.category = category;
+  }
+
+  const fromDate = parseDateFilter(from, 'from');
+  const toDate = parseDateFilter(to, 'to');
+
+  if (fromDate || toDate) {
+    filters.date = {};
+  }
+
+  if (fromDate) {
+    filters.date.$gte = fromDate;
+  }
+
+  if (toDate) {
+    toDate.setHours(23, 59, 59, 999);
+    filters.date.$lte = toDate;
+  }
+
+  if (fromDate && toDate && fromDate > toDate) {
+    const error = new Error('from date cannot be after to date');
+    error.statusCode = 400;
+    throw error;
+  }
+
+  return filters;
+};
+
+export const getExpenses = async (req, res) => {
   try {
-    const expenses = await Expense.find().sort({ date: -1, createdAt: -1 });
+    const filters = buildExpenseFilters(req.query);
+    const expenses = await Expense.find(filters).sort({ date: -1, createdAt: -1 });
 
     res.status(200).json({ expenses });
   } catch (error) {
-    sendError(res, 500, error.message);
+    sendError(res, error.statusCode ?? 500, error.message);
   }
 };
 
